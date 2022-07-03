b16e62f0e646007def2b3510cdf75f3564111a1a
gimhyeonjae <hyunjae1341@gmail.com>
2022-07-03 21:38:03 +0900

    Update Card Component

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,9 +1,14 @@
 import React from "react";
-
-import classes from "./Card.module.css";
+import styled from "styled-components";
 
 function Card(props) {
-  return <div className={classes.card}>{props.children}</div>;
+  return <CardDiv>{props.children}</CardDiv>;
 }
 
 export default Card;
+
+const CardDiv = styled.div`
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+`;
